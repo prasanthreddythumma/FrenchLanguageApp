bcaca1bcb7c11661be5a3ac120e1607674b46acc
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-07-08 03:10:40 -0400

    added code for buttons

--- app/src/main/java/com/example/frenchlanguageapp/MainActivity.java
+++ app/src/main/java/com/example/frenchlanguageapp/MainActivity.java
@@ -6,22 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btnRegister;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRegister = findViewById(R.id.btnRegister);
+        register = findViewById(R.id.btnRegister);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/example/frenchlanguageapp/RegistrationActivity.java
+++ app/src/main/java/com/example/frenchlanguageapp/RegistrationActivity.java
@@ -2,13 +2,64 @@ package com.example.frenchlanguageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    TextView firstName, lastName, password, email, phoneNumber;
+    Button btnLogin, btnRegistration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        firstName = (TextView)findViewById(R.id.firstName);
+        lastName = (TextView)findViewById(R.id.lastName);
+        password = (TextView)findViewById(R.id.password);
+        email = (TextView)findViewById(R.id.email);
+        phoneNumber = (TextView)findViewById(R.id.phoneNumber);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistration = (Button) findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+            }
+        });
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if(validate()){
+                   //db
+               }
+            }
+        });
+
+
+    }
+
+    private Boolean validate(){
+        Boolean val = false;
+        String fn = firstName.getText().toString();
+        String ln = lastName.getText().toString();
+        String pwd = password.getText().toString();
+        String em = email.getText().toString();
+        String ph = phoneNumber.getText().toString();
+
+        if(fn.isEmpty() || ln.isEmpty() || pwd.isEmpty() || em.isEmpty() || ph.isEmpty()){
+            Toast.makeText(this,"Enter all details!!",Toast.LENGTH_SHORT).show();
+            val = false;
+        }
+        else {
+
+            val = true;
+        }
+        return val;
     }
 }
