9b8627f359e7466abc3657ca7465916141f00704
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-07-08 15:27:25 -0400

    added beginner intermediate advanced

--- app/src/main/java/com/example/frenchlanguageapp/BeginnerActivity.java
+++ app/src/main/java/com/example/frenchlanguageapp/BeginnerActivity.java
@@ -3,13 +3,12 @@ package com.example.frenchlanguageapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class AssessmentActivity extends AppCompatActivity {
-    Button submit;
+public class BeginnerActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assessment);
+        setContentView(R.layout.activity_beginner);
     }
 }
