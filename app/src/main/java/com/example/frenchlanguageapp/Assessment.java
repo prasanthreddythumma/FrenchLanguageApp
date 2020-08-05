f7da948eb6eae31b1f16170046140ea3ff37b78a
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-08-05 04:36:13 -0400

    added Assessment POJO

---
+++ app/src/main/java/com/example/frenchlanguageapp/Assessment.java
@@ -0,0 +1,56 @@
+package com.example.frenchlanguageapp;
+
+public class Assessment {
+    private String Question, Option1, Option2, Option3, Option4, answer;
+
+    public Assessment() {
+    }
+
+    public String getQuestion() {
+        return Question;
+    }
+
+    public void setQuestion(String Question) {
+        this.Question = Question;
+    }
+
+    public String getOption1() {
+        return Option1;
+    }
+
+    public void setOption1(String Option1) {
+        this.Option1 = Option1;
+    }
+
+    public String getOption2() {
+        return Option2;
+    }
+
+    public void setOption2(String Option2) {
+        this.Option2 = Option2;
+    }
+
+    public String getOption3() {
+        return Option3;
+    }
+
+    public void setOption3(String Option3) {
+        this.Option3 = Option3;
+    }
+
+    public String getOption4() {
+        return Option4;
+    }
+
+    public void setOption4(String Option4) {
+        this.Option4 = Option4;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
