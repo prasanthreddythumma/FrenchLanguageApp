1fe7329c70e85c0ed65cd8f158e743f01100df84
sanjeev <sanjeevyerramsetty@gmail.com>
2020-08-05 05:00:18 -0400

    Registration fragment backend

--- app/src/main/java/com/example/frenchlanguageapp/RegisterFragment.java
+++ app/src/main/java/com/example/frenchlanguageapp/RegisterFragment.java
@@ -2,57 +2,54 @@ package com.example.frenchlanguageapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RegisterFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.HashMap;
+import java.util.Map;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class RegisterFragment extends Fragment  implements View.OnClickListener{
+    EditText edt_fName,edt_lName,edt_email,edt_pass,edt_phone;
+    Button btn_reg;
+    TextView txt_login;
+    NavController navController;
+    String fName,lName,email,pass,phone;
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
     public RegisterFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        auth=FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
+
     }
 
     @Override
@@ -61,4 +58,130 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_reg=view.findViewById(R.id.btn_registration);
+        txt_login=view.findViewById(R.id.txt_regLogin);
+        edt_fName=view.findViewById(R.id.reg_fname);
+        edt_lName=view.findViewById(R.id.reg_lname);
+        edt_email=view.findViewById(R.id.reg_email);
+        edt_pass=view.findViewById(R.id.reg_pass);
+        edt_phone = view.findViewById(R.id.reg_mobile);
+        btn_reg.setOnClickListener(this);
+        txt_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id=v.getId();
+        if(id==R.id.btn_registration){
+            fName=edt_fName.getText().toString();
+            lName=edt_lName.getText().toString();
+            email=edt_email.getText().toString();
+            pass=edt_pass.getText().toString();
+            phone=edt_phone.getText().toString();
+            if(!checkEmptyField()){
+                if(pass.length()<6){
+                    edt_pass.setError("Password must be a minimum of 6 characters");
+                    edt_pass.requestFocus();
+                    return;
+                }else{
+                    register();
+                }
+            }else{
+                return;
+            }
+        }else if(id==R.id.txt_regLogin){
+            navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+            navController.navigate(R.id.loginFragment);
+
+        }
+    }
+
+
+    public  boolean checkEmptyField(){
+        if(TextUtils.isEmpty(email)){
+            edt_email.setError("Email cannot be blank!");
+            edt_email.requestFocus();
+            return true;
+        }else if(TextUtils.isEmpty(pass)){
+            edt_pass.setError("Password cannot be blank!");
+            edt_pass.requestFocus();
+            return true;
+        }else if(TextUtils.isEmpty(phone)){
+            edt_phone.setError("phone number cannot be blank!");
+            edt_phone.requestFocus();
+            return true;
+        }else if(TextUtils.isEmpty(fName)){
+            edt_fName.setError("First Name cannot be blank!");
+            edt_fName.requestFocus();
+            return true;
+        }else if(TextUtils.isEmpty(lName)){
+            edt_lName.setError("Last Name cannot be blank!");
+            edt_lName.requestFocus();
+            return true;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edt_email.setError("Wrong Email Format!");
+            edt_email.getText().clear();
+            edt_email.requestFocus();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void register(){
+
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    try{
+                        throw task.getException();
+                    } catch (FirebaseAuthUserCollisionException already) {
+                        Toast.makeText(getActivity().getApplicationContext(),"User Already Exist!",Toast.LENGTH_LONG).show();
+                        navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                        navController.navigate(R.id.loginFragment);
+                    }
+                    catch (Exception e){
+                        Log.d("Exception", "onComplete: " + e.getMessage());
+                        Toast.makeText(getActivity().getApplicationContext(),"Register Failed!",Toast.LENGTH_LONG).show();
+                    }
+                    edt_email.getText().clear();
+                    edt_pass.getText().clear();
+                    edt_phone.getText().clear();
+                    edt_fName.getText().clear();
+                    edt_lName.getText().clear();
+                    edt_email.requestFocus();
+                    return;
+                }else{
+                    FirebaseUser user=auth.getCurrentUser();
+
+                    Map<String,Object> usermap=new HashMap<>();
+                    usermap.put("First Name",fName);
+                    usermap.put("Last Name",lName);
+                    usermap.put("Email",email);
+                    int a=0;
+                    usermap.put("Level",a);
+                    db.collection("Users").document(user.getUid()).set(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getActivity().getApplicationContext(),"Register Success!",Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                    });
+                    navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                    navController.navigate(R.id.selectFragment);
+                }
+            }
+        });
+
+    }
+
+
 }
