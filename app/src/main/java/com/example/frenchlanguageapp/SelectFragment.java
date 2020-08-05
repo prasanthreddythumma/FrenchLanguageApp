ce4029f6ce8f74d6d5362b141c8a68766ea988b2
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-08-05 04:58:26 -0400

    added SelectFragment class

--- app/src/main/java/com/example/frenchlanguageapp/SelectFragment.java
+++ app/src/main/java/com/example/frenchlanguageapp/SelectFragment.java
@@ -2,57 +2,41 @@ package com.example.frenchlanguageapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SelectFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SelectFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class SelectFragment extends Fragment implements View.OnClickListener {
+    private FirebaseAuth auth;
+    private FirebaseUser curUser;
+    public NavController navController;
+    private FirebaseFirestore db;
+    private int a;
 
     public SelectFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SelectFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SelectFragment newInstance(String param1, String param2) {
-        SelectFragment fragment = new SelectFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -60,5 +44,29 @@ public class SelectFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_select, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn_Select = view.findViewById(R.id.btn_lvl);
+        TextView txt_assess = view.findViewById(R.id.txt_assess);
+        txt_assess.setOnClickListener(this);
+        btn_Select.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.btn_lvl) {
+        }
+        else if(id == R.id.txt_assess){
+            Fragment assessFragment = new AssessFragment();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.nav_host_fragment, assessFragment ); // give your fragment container id in first parameter
+            transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+            transaction.commit();
+        }
     }
 }
