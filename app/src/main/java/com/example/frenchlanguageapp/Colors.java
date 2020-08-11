37018a30eac96b79ec931b255fbfd25edd10d1ed
sanjeev <sanjeevyerramsetty@gmail.com>
2020-08-11 08:04:40 -0400

    Colors.java

---
+++ app/src/main/java/com/example/frenchlanguageapp/Colors.java
@@ -0,0 +1,36 @@
+package com.example.frenchlanguageapp;
+
+public class Colors {
+    private String Red,Orange,Yellow,Green,Blue,Indigo,Violet;
+
+    public Colors() {
+    }
+
+    public String getRed() {
+        return Red;
+    }
+
+    public String getOrange() {
+        return Orange;
+    }
+
+    public String getYellow() {
+        return Yellow;
+    }
+
+    public String getGreen() {
+        return Green;
+    }
+
+    public String getBlue() {
+        return Blue;
+    }
+
+    public String getIndigo() {
+        return Indigo;
+    }
+
+    public String getViolet() {
+        return Violet;
+    }
+}
