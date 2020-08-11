d74e27aac8e4e5fcef1d865eb46b42bcf132217a
sanjeev <sanjeevyerramsetty@gmail.com>
2020-08-11 05:04:53 -0400

    Numbers class

---
+++ app/src/main/java/com/example/frenchlanguageapp/Numbers.java
@@ -0,0 +1,48 @@
+package com.example.frenchlanguageapp;
+
+public class Numbers {
+    private String one,two,three,four,five,six,seven,eight,nine,ten;
+
+    public Numbers() {
+    }
+
+    public String getOne() {
+        return one;
+    }
+
+    public String getTwo() {
+        return two;
+    }
+
+    public String getThree() {
+        return three;
+    }
+
+    public String getFour() {
+        return four;
+    }
+
+    public String getFive() {
+        return five;
+    }
+
+    public String getSix() {
+        return six;
+    }
+
+    public String getSeven() {
+        return seven;
+    }
+
+    public String getEight() {
+        return eight;
+    }
+
+    public String getNine() {
+        return nine;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+}
