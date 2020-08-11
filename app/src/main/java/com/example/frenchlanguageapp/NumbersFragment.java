b13ef2ff16edc092af88d13c59b8222ac3382c2e
Mounikachoudary1113 <mounicamouni1113@gmail.com>
2020-08-11 08:15:13 -0400

    added code to NumbersFragment

--- app/src/main/java/com/example/frenchlanguageapp/NumbersFragment.java
+++ app/src/main/java/com/example/frenchlanguageapp/NumbersFragment.java
@@ -2,57 +2,102 @@ package com.example.frenchlanguageapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NumbersFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class NumbersFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    TextView one,two,three,four,five,six,seven,eight,nine,ten;
+    TextView oneE,twoE,threeE,fourE,fiveE,sixE,sevenE,eightE,nineE,tenE;
+    DatabaseReference databaseReference;
+    NavController navController;
     public NumbersFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NumbersFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NumbersFragment newInstance(String param1, String param2) {
-        NumbersFragment fragment = new NumbersFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        one = view.findViewById(R.id.oneF);
+        two = view.findViewById(R.id.twoF);
+        three = view.findViewById(R.id.threeF);
+        four = view.findViewById(R.id.fourF);
+        five = view.findViewById(R.id.fiveF);
+        six = view.findViewById(R.id.sixF);
+        seven = view.findViewById(R.id.sevenF);
+        eight = view.findViewById(R.id.eightF);
+        nine = view.findViewById(R.id.nineF);
+        ten = view.findViewById(R.id.tenF);
+
+        oneE = view.findViewById(R.id.oneE);
+        twoE = view.findViewById(R.id.twoE);
+        threeE = view.findViewById(R.id.threeE);
+        fourE = view.findViewById(R.id.fourE);
+        fiveE = view.findViewById(R.id.fiveE);
+        sixE = view.findViewById(R.id.sixE);
+        sevenE = view.findViewById(R.id.sevenE);
+        eightE = view.findViewById(R.id.eightE);
+        nineE = view.findViewById(R.id.nineE);
+        tenE = view.findViewById(R.id.tenE);
+
+
+        getNumbers();
+
+    }
+
+    private void getNumbers() {
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Numbers");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final Numbers numbers = snapshot.getValue(Numbers.class);
+                one.setText(numbers.getOne());
+                two.setText(numbers.getTwo());
+                three.setText(numbers.getThree());
+                four.setText(numbers.getFour());
+                five.setText(numbers.getFive());
+                six.setText(numbers.getSix());
+                seven.setText(numbers.getSeven());
+                eight.setText(numbers.getEight());
+                nine.setText(numbers.getNine());
+                ten.setText(numbers.getTen());
+
+                oneE.setText("One");
+                twoE.setText("Two");
+                threeE.setText("Three");
+                fourE.setText("Four");
+                fiveE.setText("Five");
+                sixE.setText("Six");
+                sevenE.setText("Seven");
+                eightE.setText("Eight");
+                nineE.setText("Nine");
+                tenE.setText("Ten");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
