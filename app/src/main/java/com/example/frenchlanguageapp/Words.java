593233d2337ab0dabd73c4af1f8fbedceba2f2c6
Mounikachoudary1113 <mounicamouni1113@gmail.com>
2020-08-11 05:06:50 -0400

    Words class

---
+++ app/src/main/java/com/example/frenchlanguageapp/Words.java
@@ -0,0 +1,48 @@
+package com.example.frenchlanguageapp;
+
+public class Words {
+    private String girl,boy,woman,man,love,please,time,day,world,strong;
+
+    public Words() {
+    }
+
+    public String getGirl() {
+        return girl;
+    }
+
+    public String getBoy() {
+        return boy;
+    }
+
+    public String getWoman() {
+        return woman;
+    }
+
+    public String getMan() {
+        return man;
+    }
+
+    public String getLove() {
+        return love;
+    }
+
+    public String getPlease() {
+        return please;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public String getStrong() {
+        return strong;
+    }
+}
