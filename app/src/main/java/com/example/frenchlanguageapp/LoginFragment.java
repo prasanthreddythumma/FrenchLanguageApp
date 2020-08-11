18cb6c2fdb9f8ed732c29e1a44754f0a288098dc
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-08-11 17:55:46 -0400

    made few changes to gradle

--- app/src/main/java/com/example/frenchlanguageapp/LoginFragment.java
+++ app/src/main/java/com/example/frenchlanguageapp/LoginFragment.java
@@ -36,10 +36,11 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     private FirebaseAuth auth;
     private FirebaseUser curUser;
     public NavController navController;
-    private EditText edt_email,edt_pass;
+    private EditText edt_email, edt_pass;
     private FirebaseFirestore db;
     private int a;
-    private static final String TAG="LoginFragment";
+    private static final String TAG = "LoginFragment";
+
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -49,8 +50,8 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        auth=FirebaseAuth.getInstance();
-        db=FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -65,8 +66,8 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         super.onViewCreated(view, savedInstanceState);
         Button btn_login = view.findViewById(R.id.btn_login);
         TextView txt_reg = view.findViewById(R.id.txt_loginRegister);
-        edt_email=view.findViewById(R.id.edit_loginEmail);
-        edt_pass=view.findViewById(R.id.edit_loginPass);
+        edt_email = view.findViewById(R.id.edit_loginEmail);
+        edt_pass = view.findViewById(R.id.edit_loginPass);
         txt_reg.setOnClickListener(this);
         btn_login.setOnClickListener(this);
     }
@@ -74,93 +75,89 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        int id=v.getId();
+        int id = v.getId();
         String email = edt_email.getText().toString();
         String pass = edt_pass.getText().toString();
-        if(id==R.id.btn_login){
+        if (id == R.id.btn_login) {
             if (TextUtils.isEmpty(email)) {
                 edt_email.setError("Email cannot be blank!");
                 edt_email.requestFocus();
-            }else if(TextUtils.isEmpty(pass)){
+            } else if (TextUtils.isEmpty(pass)) {
                 edt_pass.setError("Password cannot be blank!");
                 edt_pass.requestFocus();
-            }else{
+            } else {
                 loginUser(email, pass);
             }
-        }else if(id==R.id.txt_loginRegister) {
-            navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+        } else if (id == R.id.txt_loginRegister) {
+            navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.registerFragment);
         }
     }
 
-    private void readData(final FirestoreCallback firestoreCallback){
-        DocumentReference docref=db.collection("Users").document(curUser.getUid());
+    private void readData(final FirestoreCallback firestoreCallback) {
+        DocumentReference docref = db.collection("Users").document(curUser.getUid());
         docref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if(documentSnapshot.exists()){
+                if (documentSnapshot.exists()) {
                     documentSnapshot.getData();
-                    a=(int)(long)documentSnapshot.get("Level");
-                    Log.d("readDatabase","a="+ a);
+                    a = (int) (long) documentSnapshot.get("Level");
                     firestoreCallback.onClickback(documentSnapshot);
-                }else{
-                    Log.d("Else=","Doc not exist");
+                } else {
                 }
             }
         });
     }
 
-    private interface FirestoreCallback{
+    private interface FirestoreCallback {
         void onClickback(DocumentSnapshot documentSnapshot);
     }
 
 
-    private void loginUser(String email, String pass){
-        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+    private void loginUser(String email, String pass) {
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    curUser=auth.getCurrentUser();
+                if (task.isSuccessful()) {
+                    curUser = auth.getCurrentUser();
                     readData(new FirestoreCallback() {
                         @Override
                         public void onClickback(DocumentSnapshot documentSnapshot) {
-                            if(a>0&&a<4){
-                                Toast.makeText(getActivity().getApplicationContext(),"Login Success!",Toast.LENGTH_LONG).show();
-                                if(a ==1){
-                                    navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                            if (a > 0 && a < 4) {
+                                Toast.makeText(getActivity().getApplicationContext(), "Login Success!", Toast.LENGTH_LONG).show();
+                                if (a == 1) {
+                                    navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
                                     navController.navigate(R.id.beginnersFragment);
-                                }
-                                else if(a==2){
-                                    navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                                } else if (a == 2) {
+                                    navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
                                     navController.navigate(R.id.intermediateFragment);
-                                }
-                                else if(a == 3){
-                                    navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                                } else if (a == 3) {
+                                    navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
                                     navController.navigate(R.id.advancedFragment);
                                 }
-                            }else{
-                                navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
-                                Toast.makeText(getActivity().getApplicationContext(),"First select the level",Toast.LENGTH_LONG).show();
+                            } else {
+                                navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                                Toast.makeText(getActivity().getApplicationContext(), "First select the level", Toast.LENGTH_LONG).show();
                                 navController.navigate(R.id.selectFragment);
                             }
                         }
                     });
-                }else{
-                    try{
+                } else {
+                    try {
                         throw task.getException();
-                    }catch (FirebaseAuthInvalidUserException e){
-                        Toast.makeText(getActivity().getApplicationContext(),"Email not exist!",Toast.LENGTH_LONG).show();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Email not exist!", Toast.LENGTH_LONG).show();
                         edt_email.getText().clear();
                         edt_pass.getText().clear();
                         edt_email.setError("Email not exist!");
                         edt_email.requestFocus();
-                    }catch (FirebaseAuthInvalidCredentialsException e){
-                        Toast.makeText(getActivity().getApplicationContext(),"Wrong Password!",Toast.LENGTH_LONG).show();
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Wrong Password!", Toast.LENGTH_LONG).show();
                         edt_pass.getText().clear();
                         edt_pass.setError("Wrong Password!");
                         edt_pass.requestFocus();
-                    }catch (Exception e ){
-                        Toast.makeText(getActivity().getApplicationContext(),"Login Failed!",Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Login Failed!", Toast.LENGTH_LONG).show();
                     }
                 }
             }
@@ -171,27 +168,14 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onStart() {
         super.onStart();
-        Log.d(TAG,"onStart Called!! "+a);
-        // Log.d("a=","value is"+a);
-        curUser=auth.getCurrentUser();
-        if(curUser!=null){
-            Toast.makeText(getActivity().getApplicationContext(),"User Already Login",Toast.LENGTH_LONG).show();
-            if(a>0&&a<4){
+        curUser = auth.getCurrentUser();
+        if (curUser != null) {
+            Toast.makeText(getActivity().getApplicationContext(), "User Already Login", Toast.LENGTH_LONG).show();
 
-                if(a ==1){
-                    Toast.makeText(getActivity().getApplicationContext(),"Beginners!",Toast.LENGTH_LONG).show();
-                }
-                else if(a==2){
-                    Toast.makeText(getActivity().getApplicationContext(),"Intermediate!",Toast.LENGTH_LONG).show();
-                }
-                else if(a == 3){
-                    Toast.makeText(getActivity().getApplicationContext(),"Advanced!",Toast.LENGTH_LONG).show();
-                }
-            }else{
-                navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
-                Toast.makeText(getActivity().getApplicationContext(),"First select the level",Toast.LENGTH_LONG).show();
-                navController.navigate(R.id.selectFragment);
-            }
+            navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+            Toast.makeText(getActivity().getApplicationContext(), "First select the level", Toast.LENGTH_LONG).show();
+            navController.navigate(R.id.selectFragment);
+
 
         }
     }
