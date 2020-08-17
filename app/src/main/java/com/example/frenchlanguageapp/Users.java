8002d745aa674cd98d3b7af8fc8606f494a53104
prasanthreddythumma <prasanthreddy770@gmail.com>
2020-08-17 03:09:45 -0700

    Added Users POJO

---
+++ app/src/main/java/com/example/frenchlanguageapp/Users.java
@@ -0,0 +1,45 @@
+package com.example.frenchlanguageapp;
+
+public class Users {
+    String FirstName,LastName,Password,EMail,PhoneNumber;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEMail() {
+        return EMail;
+    }
+
+    public void setEMail(String EMail) {
+        this.EMail = EMail;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+}
