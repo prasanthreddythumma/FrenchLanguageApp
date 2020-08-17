318025cc0019b0828299a0a37040fe64eeedc15e
sanjeev <sanjeevyerramsetty@gmail.com>
2020-08-17 02:17:31 -0400

    added Phrases POJO

---
+++ app/src/main/java/com/example/frenchlanguageapp/Phrases.java
@@ -0,0 +1,49 @@
+package com.example.frenchlanguageapp;
+
+public class Phrases {
+    private String P1,P2,P3,P4,P5,P6,P7,P8,P9,P10;
+
+
+    public Phrases() {
+    }
+
+    public String getP1() {
+        return P1;
+    }
+
+    public String getP2() {
+        return P2;
+    }
+
+    public String getP3() {
+        return P3;
+    }
+
+    public String getP4() {
+        return P4;
+    }
+
+    public String getP5() {
+        return P5;
+    }
+
+    public String getP6() {
+        return P6;
+    }
+
+    public String getP7() {
+        return P7;
+    }
+
+    public String getP8() {
+        return P8;
+    }
+
+    public String getP9() {
+        return P9;
+    }
+
+    public String getP10() {
+        return P10;
+    }
+}
