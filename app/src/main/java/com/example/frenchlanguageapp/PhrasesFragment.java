112a21aeea960743bfc32067a4cee55100ae1cf7
sanjeev <sanjeevyerramsetty@gmail.com>
2020-08-17 02:19:07 -0400

    added implementation to view PhrasesFragment

--- app/src/main/java/com/example/frenchlanguageapp/PhrasesFragment.java
+++ app/src/main/java/com/example/frenchlanguageapp/PhrasesFragment.java
@@ -2,57 +2,80 @@ package com.example.frenchlanguageapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PhrasesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class PhrasesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView P1,P2,P3,P4,P5,P6,P7,P8,P9,P10;
+    DatabaseReference databaseReference;
 
     public PhrasesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PhrasesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PhrasesFragment newInstance(String param1, String param2) {
-        PhrasesFragment fragment = new PhrasesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        P1 = view.findViewById(R.id.pF1);
+        P2 = view.findViewById(R.id.pF2);
+        P3 = view.findViewById(R.id.pF3);
+        P4 = view.findViewById(R.id.pF4);
+        P5 = view.findViewById(R.id.pF5);
+        P6 = view.findViewById(R.id.pF6);
+        P7 = view.findViewById(R.id.pF7);
+        P8 = view.findViewById(R.id.pF8);
+        P9 = view.findViewById(R.id.pF9);
+        P10 = view.findViewById(R.id.pF10);
+
+        getPhrases();
+    }
+
+    private void getPhrases() {
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Phrases");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final Phrases phrases = snapshot.getValue(Phrases.class);
+                P1.setText(phrases.getP1());
+                P2.setText(phrases.getP2());
+                P3.setText(phrases.getP3());
+                P4.setText(phrases.getP4());
+                P5.setText(phrases.getP5());
+                P6.setText(phrases.getP6());
+                P7.setText(phrases.getP7());
+                P8.setText(phrases.getP8());
+                P9.setText(phrases.getP9());
+                P10.setText(phrases.getP10());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
